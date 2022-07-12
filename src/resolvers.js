040e8637dc50093f3be74e195d6df3bca17bcfcf
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-07-12 23:15:24 +0100

    suprememods

--- src/resolvers.js
+++ src/resolvers.js
@@ -45,14 +45,14 @@ const resolvers = {
       return dataSources.trackAPI.getTrackModules(id);
     },
 
-    durationInSeconds: ({ length }) => {
-      length
-    },
+    durationInSeconds: ({ length }) => length,
 
-    Module: {
-      durationInSeconds: ({ length }) => length
-    }
   },
+
+  Module: {
+    durationInSeconds: ({ length }) => length
+  }
+  
 };
 
 module.exports = resolvers;
